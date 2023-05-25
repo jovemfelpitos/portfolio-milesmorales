0162bd32f0788a82cec87d617e2b4957b53eb737
Felpitos <109309830+jovemfelps@users.noreply.github.com>
2023-05-25 18:01:07 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+    function menuShow() {
+        let menuMobile = document.querySelector('.mobile-cabecalho')
+        if (menuMobile.classList.contains('open')){
+            menuMobile.classList.remove('open')
+            document.querySelector('.icon').src = "assets/icon-menu.png"
+        } else {
+            menuMobile.classList.add('open')
+            document.querySelector('.icon').src = "assets/icon-fechar.png"
+        }
+    }
