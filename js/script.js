9500d7372ec60770c9e97bfd327e296418a8649c
Felpitos <109309830+jovemfelpitos@users.noreply.github.com>
2023-05-26 11:38:26 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
         let menuMobile = document.querySelector('.mobile-cabecalho')
         if (menuMobile.classList.contains('open')){
             menuMobile.classList.remove('open')
-            document.querySelector('.icon').src = "assets/icon-menu.png"
+            document.querySelector('.icon').src = "assets/menu-icon.png"
         } else {
             menuMobile.classList.add('open')
             document.querySelector('.icon').src = "assets/icon-fechar.png"
